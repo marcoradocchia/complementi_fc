2e43ceeb25c9813e9fb5993074248a5ccdf8ad27
marcoradocchia <74802223+marcoradocchia@users.noreply.github.com>
2021-02-07 18:52:55 +0100

    exit intersection working

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from shape import Shape
 from walker import Walker
 
 def main():
-	myShape = Shape(radius=5, vertsNum=6)
+	myShape = Shape(radius=5, vertsNum=300)
 	myWalker = Walker(shapeVerts=myShape.verts, startingPos=myShape.getStartingPos())
 
 	#GET RID IN THE FINAL VERSION
@@ -10,15 +10,16 @@ def main():
 		for vert in myShape.verts:
 			outFile.write('{}, {}\n'.format(vert[0], vert[1]))
 		outFile.write('\n')
+
 		while True: #walking looper
 			outFile.write('{}, {}\n'.format(myWalker.pos[0], myWalker.pos[1]))
 			myWalker.move()
-			if not myWalker.checkInside(): break
-
-		#TODO: IMPLEMENT INTERSECTION POINT ON EXIT
-		myWalker.getIntersectionPoint()
+			if not myWalker.checkInside():
+				myWalker.getIntersection()
+				outFile.write('{}, {}\n'.format(myWalker.pos[0], myWalker.pos[1]))
+				break
 		
-		outFile.write('{}, {}\n'.format(myWalker.pos[0], myWalker.pos[1]))
+		print('Moves: ', myWalker.moves)
 
 
 if __name__ == '__main__':
--- src/walker.py
+++ src/walker.py
@@ -1,18 +1,20 @@
 from math import pi, cos, sin
 from random import uniform, normalvariate
 
+tolerance = 1e-5
+
 def getStepLen():
 	return normalvariate(mu=1, sigma=0.2)
 
-def firstMove(): #TODO: IMPLEMENT FIRST MOVE
-	length = getStepLen()
-	return length
+def firstMove(walker, startingPos): #TODO: IMPLEMENT FIRST MOVE
+	pass
 
 class Walker():
 	def __init__(self, shapeVerts, startingPos):
 		self.shapeVerts = shapeVerts
 		self.pos = startingPos
-		self.distance = firstMove()
+		self.distance = 0
+		self.moves = 0
 		self.prevPos = None
 
 	def move(self):
@@ -22,32 +24,38 @@ class Walker():
 		self.pos[0] += length * cos(direction)
 		self.pos[1] += length * sin(direction)
 		self.distance += length
+		self.moves += 1
 
 	def checkInside(self): #uses even-odd rule and returns true if the point's inside, false otherwise
 		x = self.pos[0]
 		y = self.pos[1]
 		pts = self.shapeVerts
-		j = len(self.shapeVerts) - 1
-		isOut = False
+		j = len(pts) - 1
+		isIn = False
 		for i in range(len(pts)):
 			if ((pts[i][1] > y) != (pts[j][1] > y)) and (x < pts[i][0] + (pts[j][0] - pts[i][0]) * (y - pts[i][1]) / (pts[j][1] - pts[i][1])):
-				isOut = not isOut
+				isIn = not isIn
 			j = i
-		return isOut
+		return isIn
 
-	def getIntersectionPoint(self): #working
+	def getIntersection(self): #not working properly with high number of vertices
 		pts = self.shapeVerts
-		x2 = self.pos[0]
-		y2 = self.pos[1]
 		x1 = self.prevPos[0]
 		y1 = self.prevPos[1]
+		x2 = self.pos[0]
+		y2 = self.pos[1]
 		for i in range(len(pts) - 1):
 			x3 = pts[i][0]
 			y3 = pts[i][1]
 			x4 = pts[i + 1][0]
 			y4 = pts[i + 1][1]
-			t = ((x4 - x3) * (y1 - y3) - (x1 - x3) * (y4 - y3)) / ((x2 - x1) * (y4 - y3) - (y2 - y1) * (x4 - x3))
-			if t <= 1 and t >= 0:
-				self.pos[0] = x1 + t * (x2 - x1)
-				self.pos[1] = y1 + t * (y2 - y1)
-				break
+			den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+			if den == 0: continue
+			t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
+			u = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / den
+			if t > tolerance and t < 1 - tolerance and u > tolerance and u < 1 - tolerance:
+				x = x1 + t * (x2 - x1)
+				y = y1 + t * (y2 - y1)
+				self.pos = [x, y]
+				print('INTERSECTION FOUND', i+1)
+				return
